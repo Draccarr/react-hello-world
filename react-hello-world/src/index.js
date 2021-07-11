a1f6d1dfd8b715268a1ea4428b713476edb65ffb
Gavenmore <jamesdon10@hotmail.com>
2021-07-11 00:51:58 -0600

    Added input field

--- react-hello-world/src/index.js
+++ react-hello-world/src/index.js
@@ -1,18 +1,20 @@
 import React, {useState, useEffect} from 'react';
 import ReactDOM from 'react-dom';
+import { makeStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
+import Icon from '@material-ui/core/Icon';
 import './index.css';
+import './github-user-info.css';
 import reportWebVitals from './reportWebVitals';
 
-const userCardStyle = {
-  display: 'flex',
-  flexDirection: 'row',
-  alignItems: 'center',
-  color: '#ccc',
-  backgroundColor: '#141414',
-  borderRadius: '1rem',
-  margin: '2rem',
-  padding: '.5rem 1rem',
-};
+const useStyles = makeStyles( 
+  (_theme) => ({
+    button: {
+      margin: _theme.spacing(1)
+    }
+  })
+);
 
 function GitHubUserCard({username}) {
   const [data, setData] = useState(null);
@@ -26,7 +28,7 @@ function GitHubUserCard({username}) {
 
   if (data) {
     elementToReturn = 
-      <section className="user-card" style={userCardStyle}>
+      <section className="github-user-card">
         <h2 style={{marginRight: '2rem'}}><a href={data.blog}>{data.login}</a></h2>
         <img src={data.avatar_url} width='100'/>
       </section>
@@ -34,8 +36,27 @@ function GitHubUserCard({username}) {
   return elementToReturn;
 }//end GitHubUser Component
 
+function GitHubForm(_properties) {
+  const classes = useStyles();
+  return (
+    //This is a fragment
+    <>
+    <section className="git-hub-form">
+      <h2>Info</h2>
+      <TextField id="standard-basic" label="Standard" />
+    </section>
+    <Button variant="contained" color="primary" className={classes.button} endIcon={<Icon>send</Icon>} onClick={() => {console.log(";)")}}>Send</Button>
+    </>
+  );
+}//end GitHubForm Component
+
 function App() {
-  return <GitHubUserCard username="draccarr"/>
+  return (
+    <>
+      <GitHubUserCard username="draccarr"/>
+      <GitHubForm />
+    </>
+  );
 }//end App Component
 
 ReactDOM.render(
